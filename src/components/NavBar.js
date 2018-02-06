5ed0ec7192c0727cfb0ef387444a0796ebe8993e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-06 03:50:08 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   const link = {
-    width: 100px,
+    width: '100px',
     padding: '12px',
     margin: '0 6px 6px',
     background: 'blue',
